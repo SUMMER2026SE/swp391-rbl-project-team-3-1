b23d61353f322a5928f8336b68ab46c39ba6487f
minhtuedz <buiminhtue2005@gmail.com>
2026-05-18 10:54:16 +0700

    first commit

---
+++ backend/src/main/java/com/fx/fitness/FxFitnessCenterApplication.java
@@ -0,0 +1,13 @@
+package com.fx.fitness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FxFitnessCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FxFitnessCenterApplication.class, args);
+	}
+
+}
