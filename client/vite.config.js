280afbab85485eb77c79a8def5c26304da8db572
minhtuedz <buiminhtue2005@gmail.com>
2026-06-09 08:50:52 +0700

    update 4. Workout & Nutrition Module

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,28 +1,17 @@
-<<<<<<< HEAD
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-=======
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
     port: 3000,
->>>>>>> tue1
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
         changeOrigin: true,
         secure: false,
       },
-<<<<<<< HEAD
       '/uploads': {
         target: 'http://localhost:5000',
         changeOrigin: true,
@@ -30,13 +19,8 @@ export default defineConfig({
       },
     },
   },
-})
-=======
-    },
-  },
   build: {
     outDir: '../server/public',
     emptyOutDir: true, // cleans the output directory before building
   },
 });
->>>>>>> tue1
