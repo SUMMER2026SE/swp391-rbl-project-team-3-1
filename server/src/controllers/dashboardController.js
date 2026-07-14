bb01c39af45996806472541d2f8c7b139f003149
Hoang Lan <hoanglan1912@gmail.com>
2026-07-14 22:38:14 +0700

    Use GETUTCDATE() in finishMemberProgress to fix timezone shift bug

--- server/src/controllers/dashboardController.js
+++ server/src/controllers/dashboardController.js
@@ -1463,7 +1463,7 @@ exports.finishMemberProgress = async (req, res) => {
       return res.status(400).json({ message: 'Chỉ huấn luyện viên mới được thực hiện thao tác này!' });
     }
 
-    const yesterday = sequelize.literal("DATEADD(day, -1, GETDATE())");
+    const yesterday = sequelize.literal("DATEADD(day, -1, GETUTCDATE())");
 
     // 1. Move all workout plans of this member to history
     await models.WorkoutPlans.update(
